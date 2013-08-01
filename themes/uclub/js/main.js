58d7198f4dd280ab8ac9513bf811d9b923232bb4
Lev Cantoral <meekar@gmail.com>
2013-08-01 14:15:09 -0500

    room page type, flex slider

--- themes/uclub/js/main.js
+++ themes/uclub/js/main.js
@@ -3,10 +3,18 @@ $(window).load(function() {
     //$('body').addClass('js');
 
 	// Flexslider
-	// $('.flexslider').flexslider({
-	// 	slideshow: false
-	// });
 
+      $('.flexslider').show();
+
+       //Flexslider
+      $('.flexslider').flexslider({
+            animation: "slide",
+            animationLoop: true,
+            itemMargin: 0,
+            minItems: 1,
+            maxItems: 1,
+            itemWidth: 500,
+      });
 	$('.nav-title a').click(function() {
 		$('.nav-main-wrapper').toggleClass('expand');
 		return false;
