ad9efa1ffd56901ba67c2b054a2dd29daa39ae7d
Dustin Quam <quamsta@gmail.com>
2018-05-08 14:05:04 -0500

    updates

--- mysite/_config.php
+++ mysite/_config.php
@@ -11,7 +11,7 @@ MySQLDatabase::set_connection_charset('utf8');
 
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
+SSViewer::set_theme('division-subtheme');
 
 // Set the site locale
 i18n::set_locale('en_US');
